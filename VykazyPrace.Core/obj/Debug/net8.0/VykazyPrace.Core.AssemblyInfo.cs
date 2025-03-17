77f5cdca620046ea767e7af0e54e64398cd84ee4
johnnyprochazka <jan.prochazka@heinz-glas.com>
2025-03-17 08:13:31 +0100

    database update

--- VykazyPrace.Core/obj/Debug/net8.0/VykazyPrace.Core.AssemblyInfo.cs
+++ VykazyPrace.Core/obj/Debug/net8.0/VykazyPrace.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VykazyPrace.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a84dc4d96bc16248e70309e637a3e04e99b7534")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9aab19b990a8981ecdd7bd8a55b5dd90189d118")]
 [assembly: System.Reflection.AssemblyProductAttribute("VykazyPrace.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VykazyPrace.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

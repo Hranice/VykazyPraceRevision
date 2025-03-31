bcf0c42045b13ddcd70a039459818c93bf559b1c
Jan Procházka <Jan.Prochazka@heinz-glas.com>
2025-03-31 12:04:28 +0200

    bugfix project filtered selection

--- VykazyPrace.Core/obj/Debug/net8.0/VykazyPrace.Core.AssemblyInfo.cs
+++ VykazyPrace.Core/obj/Debug/net8.0/VykazyPrace.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VykazyPrace.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa0431f7acd465d6c92246c3f30d43b8fd062c27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+247b3a0353ce5ea4fde7c99c47cd144fb8b6c152")]
 [assembly: System.Reflection.AssemblyProductAttribute("VykazyPrace.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VykazyPrace.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

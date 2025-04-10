8ea47f9253577d9f278812b05a23998c17145f97
Jan Procházka <Jan.Prochazka@heinz-glas.com>
2025-04-10 08:49:39 +0200

    cleanup & project fix

--- VykazyPrace.Core/obj/Debug/net8.0/VykazyPrace.Core.AssemblyInfo.cs
+++ VykazyPrace.Core/obj/Debug/net8.0/VykazyPrace.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VykazyPrace.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+338fe0dc348ff93dc6a7ddd9efb2ae51240c1f0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56c5ca33ab1fa605259442fca11ad2960debbab9")]
 [assembly: System.Reflection.AssemblyProductAttribute("VykazyPrace.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VykazyPrace.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

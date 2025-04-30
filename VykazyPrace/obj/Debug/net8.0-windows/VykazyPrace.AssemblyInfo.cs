f4bbee9680a77ac13fcb88b9f3cb8ff780dea437
Jan Procházka <Jan.Prochazka@heinz-glas.com>
2025-04-30 10:55:51 +0200

    finally export done

--- VykazyPrace/obj/Debug/net8.0-windows/VykazyPrace.AssemblyInfo.cs
+++ VykazyPrace/obj/Debug/net8.0-windows/VykazyPrace.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("© 2025 Jan Procházka")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Výkazy hodin")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.8.1")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.8.1+e875ef7c90db750746f5e82b1765a3d6317b2d42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.8.1+bae0019f477d95d084d7b25c2aa6b23e3098b8a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkLog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkLog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.1.8.1")]

2b59d3e28ff2d32df20c647cd3400858727cdd02
Jan Procházka <Jan.Prochazka@heinz-glas.com>
2025-05-15 12:03:14 +0200

    release publish

--- VykazyPrace.Core/obj/Release/net8.0/VykazyPrace.Core.AssemblyInfo.cs
+++ VykazyPrace.Core/obj/Release/net8.0/VykazyPrace.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VykazyPrace.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3a9cf2d610cd12f014d33fa88b9a39d84717342")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91049c9ea2735fa69984c16f37e0ca10c5978f28")]
 [assembly: System.Reflection.AssemblyProductAttribute("VykazyPrace.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VykazyPrace.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

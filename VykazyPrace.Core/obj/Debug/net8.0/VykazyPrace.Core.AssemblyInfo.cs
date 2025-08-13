a0f4712277b73a1f7d7b61e2062c6736d9077bd9
Jan Procházka <Jan.Prochazka@heinz-glas.com>
2025-08-13 14:39:27 +0200

    testdialog arrivals revision

--- VykazyPrace.Core/obj/Debug/net8.0/VykazyPrace.Core.AssemblyInfo.cs
+++ VykazyPrace.Core/obj/Debug/net8.0/VykazyPrace.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VykazyPrace.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adebbce835d4957df43ea1b552696316e5838567")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16acd83b7cca7fe3e059048f42d542731b3bbe44")]
 [assembly: System.Reflection.AssemblyProductAttribute("VykazyPrace.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VykazyPrace.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WorkLogWpf/obj/Debug/net8.0-windows/WorkLogWpf.AssemblyInfo.cs
+++ WorkLogWpf/obj/Debug/net8.0-windows/WorkLogWpf.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkLogWpf")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ea8196e5383168d246e324d4002ed80ee545502")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16acd83b7cca7fe3e059048f42d542731b3bbe44")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkLogWpf")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkLogWpf")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

ef64370e6e0d834602fa2e306ffe54e09a775efb
Jan Procházka <Jan.Prochazka@heinz-glas.com>
2026-04-15 15:40:49 +0200

    1.4.2.0 (Export revision)
    Release & publish

--- VykazyPrace.Core/obj/Release/net8.0/VykazyPrace.Core.AssemblyInfo.cs
+++ VykazyPrace.Core/obj/Release/net8.0/VykazyPrace.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VykazyPrace.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5deadc1809f803436e257c1e13c575174aca371d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69816aa54571af31f50072f0aae5fe393d16a8fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("VykazyPrace.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VykazyPrace.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

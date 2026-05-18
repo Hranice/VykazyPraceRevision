523d61bd4f5115fe765aa04d1dd0d37d70856f79
Jan Procházka <Jan.Prochazka@heinz-glas.com>
2026-05-18 11:21:23 +0200

    import / change fallbackproject id

--- VykazyPrace.Core/obj/Debug/net8.0/VykazyPrace.Core.AssemblyInfo.cs
+++ VykazyPrace.Core/obj/Debug/net8.0/VykazyPrace.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VykazyPrace.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+474d63b9062a5bbf9ec392e6d4c7386954007946")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d88ec626f95d60090226c6c891f2944b819c8566")]
 [assembly: System.Reflection.AssemblyProductAttribute("VykazyPrace.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VykazyPrace.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
